574324b0d8880d627fc5cbbc89f293ef58080b36
1313ou <1313ou@gmail.com>
2024-10-18 14:44:10 +0200

    new escape lemma scheme

--- wordnet_xml.py
+++ wordnet_xml.py
@@ -62,7 +62,7 @@ custom_char_escapes = {
     '-': '--',  # custom
     ' ': '_',  # custom
     # '.': '-period-', # valid xml id char
-    #'_': '-lowbar-', # valid xml id char
+    '_': '-lowbar-', # valid xml id char
     "'": '-apos-',
     '`': '-grave-',
     '´': '-acute-',
@@ -93,6 +93,42 @@ custom_char_escapes = {
 }
 custom_char_escapes_reverse = {custom_char_escapes[k]: k for k in custom_char_escapes}
 
+custom_char_escapes_for_sk = {
+    # HTML entities
+    # https://en.wikipedia.org/wiki/List_of_XML_and_HTML_character_entity_references
+    '-': '--',  # custom
+    # '.': '-period-', # valid xml id char
+    #'_': '-lowbar-', # valid xml id char
+    "'": '-apos-',
+    '`': '-grave-',
+    '´': '-acute-',
+    '(': '-lpar-',
+    ')': '-rpar-',
+    '[': '-lsqb-',
+    ']': '-rsqb-',
+    '{': '-lbrace-',
+    '}': '-rbrace-',
+    ',': '-comma-',
+    # ':': '-colon-', # valid xml id char
+    ';': '-semi-',
+    '=': '-equals-',
+    '+': '-plus-',
+    '!': '-excl-',
+    '?': '-quest-',
+    '@': '-commat-',
+    '#': '-num-',
+    '$': '-dollar-',
+    '%': '-percnt-',
+    '/': '-sol-',
+    '\\': '-bsol-',
+    '|': '-vert-',
+    '^': '-Hat-',
+    '*': '-ast-',
+    '‘': '-lsquo-',
+    '’': '-rsquo-',
+}
+custom_char_escapes_for_sk_reverse = {custom_char_escapes_for_sk[k]: k for k in custom_char_escapes_for_sk}
+
 def escape_lemma(lemma):
     """Format the lemma so it is valid XML ID sequence"""
 
@@ -111,19 +147,49 @@ def escape_lemma(lemma):
 def unescape_lemma(lemma):
     """Format the valid XML ID sequence so it is the original lemma"""
 
-    def uelc(c):
-        if ('A' <= c <= 'Z') or ('a' <= c <= 'z') or ('0' <= c <= '9'):
-            return c
-        elif xml_id_char1_re.match(c):
-            return c
-        raise ValueError(f'{c!r} [x{ord(c):04X}] is illegal character in XML ID and no escape sequence is defined')
+    #def uelc(c):
+    #    if ('A' <= c <= 'Z') or ('a' <= c <= 'z') or ('0' <= c <= '9'):
+    #        return c
+    #    elif xml_id_char1_re.match(c):
+    #        return c
+    #    raise ValueError(f'{c!r} [x{ord(c):04X}] is illegal character in XML ID and no escape sequence is defined')
 
     s = lemma
     for seq in custom_char_escapes_reverse:
         s = s.replace(seq, custom_char_escapes_reverse[seq])
-    s = "".join(uelc(c) for c in s)
+    # s = "".join(uelc(c) for c in s)
     return s
 
+def escape_lemma_for_sk(lemma):
+    """Format the lemma so it is valid XML ID sequence"""
+
+    def elc(c):
+        if ('A' <= c <= 'Z') or ('a' <= c <= 'z') or ('0' <= c <= '9'):
+            return c
+        elif c in custom_char_escapes_for_sk:
+            return custom_char_escapes_for_sk[c]
+        elif xml_id_char1_re.match(c):
+            return c
+        raise ValueError(f'{c!r} [x{ord(c):04X}] is illegal character in XML ID and no escape sequence is defined')
+
+    return ''.join(elc(c) for c in lemma)
+
+
+def unescape_lemma_for_sk(lemma):
+    """Format the valid XML ID sequence so it is the original lemma"""
+
+    #def uelc(c):
+    #    if ('A' <= c <= 'Z') or ('a' <= c <= 'z') or ('0' <= c <= '9'):
+    #        return c
+    #    elif xml_id_char1_re.match(c):
+    #        return c
+    #    raise ValueError(f'{c!r} [x{ord(c):04X}] is illegal character in XML ID and no escape sequence is defined')
+
+    s = lemma
+    for seq in custom_char_escapes_for_sk_reverse:
+        s = s.replace(seq, custom_char_escapes_for_sk_reverse[seq])
+    # s = "".join(uelc(c) for c in s)
+    return s
 
 middledot = '·'
 xml_percent_sep = middledot
@@ -134,14 +200,14 @@ def escape_sensekey(sensekey):
     """Escape the sensekey so that it contains valid characters for XML ID, prefix added"""
     if '%' in sensekey:
         e = sensekey.split('%')
-        lemma = escape_lemma(e[0].replace('_',' '))
+        lemma = escape_lemma_for_sk(e[0])
         lex_sense = e[1]
         lex_sense_fields = lex_sense.split(':')
         n = len(lex_sense_fields)
         assert n == 5, f'Parsing error: length {n} of lex_sense_fields in lex_sense {lex_sense} should be 5'
         head = lex_sense_fields[3]
         if head:
-            lex_sense_fields[3] = escape_lemma(head)
+            lex_sense_fields[3] = escape_lemma_for_sk(head)
         return f"oewn-{lemma}{xml_percent_sep}{xml_colon_sep.join(lex_sense_fields)}"
     raise ValueError(f'Ill-formed OEWN sense key (no %): {sensekey}')
 
@@ -152,14 +218,14 @@ def unescape_sensekey(escaped_sensekey):
     """
     if xml_percent_sep in escaped_sensekey:
         e = escaped_sensekey.split(xml_percent_sep)
-        lemma = unescape_lemma(e[0][key_prefix_len:])
+        lemma = unescape_lemma_for_sk(e[0][key_prefix_len:])
         lex_sense = e[1]
         lex_sense_fields = lex_sense.split(xml_colon_sep)
         n = len(lex_sense_fields)
         assert n == 5, f'Parsing error: length {n} of lex_sense_fields in lex_sense {lex_sense} should be 5'
         head = lex_sense_fields[3]
         if head:
-            lex_sense_fields[3] = unescape_lemma(head)
+            lex_sense_fields[3] = unescape_lemma_for_sk(head)
         return f'{lemma}%{':'.join(lex_sense_fields)}'
     raise ValueError(f'Ill-formed OEWN sense key (no {xml_percent_sep}): {escaped_sensekey}')
 
