39494e28b4ef6f4380b5684bf0c7e762c0cc2694
1313ou <1313ou@gmail.com>
2024-10-27 07:08:02 +0100

    add types from pyi

--- tests/model.py
+++ tests/model.py
@@ -5,10 +5,13 @@ import os
 import sys
 
 from oewn_core import deserialize
+from typing import List, Generator
+
+from wordnet import WordnetModel, Sense
 
 data_home = os.environ['OEWN_HOME']
 print(f'data={data_home}', file=sys.stderr)
-wn = deserialize.load_pickle(data_home)
-sorted_entries = sorted(list(wn.entries), key=lambda e: e.lemma)
-senses = wn.senses
-sorted_senses = sorted(list(wn.senses), key=lambda s: s.id)
+wn: WordnetModel = deserialize.load_pickle(data_home)
+sorted_entries: list = sorted(list(wn.entries), key=lambda e: e.lemma)
+senses: Generator[Sense, None, None] = wn.senses
+sorted_senses: List[Sense] = sorted(list(wn.senses), key=lambda s: s.id)
