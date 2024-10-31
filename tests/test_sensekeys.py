5963b37039e60d1ee99e755ec5ac6f31f55f85fc
1313ou <1313ou@gmail.com>
2024-10-31 08:32:00 +0100

    revise tests

--- tests/test_sensekeys.py
+++ tests/test_sensekeys.py
@@ -10,10 +10,10 @@ Author: Bernard Bou <1313ou@gmail.com> for rewrite and revamp
 import sys
 import unittest
 
-from oewn_xml import wordnet_xml as xml
+from oewn_xml.wordnet_xml import is_valid_xml_id, to_xml_sense_id, from_xml_sense_id
 from oewn_xml.wordnet_xml import key_prefix_len
 from tests.model import sorted_senses
-from tests.utils import is_parsable_sensekey, is_parsable_xml_sensekey
+from tests.utils import is_parsable_sensekey, is_parsable_xml_sensekey, is_valid_xsd_id
 
 
 class SensekeysTestCase(unittest.TestCase):
@@ -22,13 +22,13 @@ class SensekeysTestCase(unittest.TestCase):
         for s in sorted_senses:
             try:
                 self.assertTrue(is_parsable_sensekey(s.id))
-                sid = xml.to_xml_sense_id(s.id)
-                self.assertTrue(xml.is_valid_xml_id(sid), f'{sid}')
+                sid = to_xml_sense_id(s.id)
+                self.assertTrue(is_valid_xml_id(sid), f'{sid}')
+                self.assertTrue(is_valid_xsd_id(sid), f'{sid}')
                 self.assertTrue(is_parsable_xml_sensekey(sid[key_prefix_len:]), f'{sid}')
-                sk = xml.from_xml_sense_id(sid)
+                sk = from_xml_sense_id(sid)
                 self.assertEqual(s.id, sk)
                 print(f'{s.id}   {sid}   {sk}')
-
             except ValueError as e:
                 print(e, file=sys.stderr)
 
