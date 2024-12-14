00945cace57185ab5b30c167a89a76253e18da19
1313ou <1313ou@gmail.com>
2024-12-14 11:10:49 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oewn-core',
-    version='1.2.2',
+    version='1.3.0',
     author='Bernard Bou, John McCrae',
     author_email='oewntk@gmail.com',
     description='Core library for oewn',
