d17d51f97021625b4339a4679ac9903fbac44952
treebolic <treebolic@gmail.com>
2026-09-16 06:58:36 +0200

    house keeping

--- _merge-pyi.sh
+++ _merge-pyi.sh
@@ -8,7 +8,7 @@
 
 MERGE_PYI='.venv/bin/merge-pyi -i'
 MERGE_PYI='.venv/bin/merge-pyi --diff'
-for p in oewn_core oewn_xml tests; do
+for p in oewn_core oewn_plus oewn_xml oewn_syntagnet oewn_validate tests; do
     while read -r f; do
       b=$(basename "$f");
       s="${b%.*}";
